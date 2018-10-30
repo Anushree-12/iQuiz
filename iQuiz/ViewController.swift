08d95b02f9742d42bcf4682ae631575c6c65ca39
anushree-12 <agopal12@uw.edu>
2018-10-29 17:09:38 -0700

    some errors

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -8,13 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let subjects = ["Mathematics", "Marvel Super Heroes", "Science"]
+//    let description = ["math", "heroes", "science"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return(subjects.count)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        cell.textLabel?.text = subjects[indexPath.row]
+//        cell.descriptionLabel?.text = description[indexPath.row]
+        
+        return(cell)
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    
+    @IBAction func btnSetting(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Settings", message: "Settings go here", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
 }
-
